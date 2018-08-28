125da0836e6e5737c013a504c3223a1fb1b0e3ff
Derek Silva <dereksilvamusic@gmail.com>
2018-08-28 19:43:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
+    each: function(collection, iteratee) {
+      const newCollection = (collection instanceof Array) ? collection.slice() : Object.values(collection)
 
+      for (let i = 0; i < newCollection.length; i++)
+        iteratee(newCollection[i])
+
+      return collection
     },
 
     map: function() {
